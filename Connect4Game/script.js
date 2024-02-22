6089d9bf873e1c040097891cc136f8e9585a5a53
mihirsahu36 <123735395+mihirsahu36@users.noreply.github.com>
2024-02-23 00:14:25 +0530

    Add files via upload

---
+++ Connect4Game/script.js
@@ -0,0 +1,140 @@
+var playerRed = "R";
+var playerBlue = "B";
+var currPlayer = playerRed;
+
+var gameOver = false;
+var board;
+
+var rows = 6;
+var columns = 7;
+var currColumns = [];
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(' ');
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
+        }
+        board.push(row);
+    }
+}
+
+function setPiece() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    r = currColumns[c]; 
+
+    if (r < 0) {
+        return;
+    }
+
+    board[r][c] = currPlayer;
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if (currPlayer == playerRed) {
+        tile.classList.add("red-piece");
+        currPlayer = playerBlue;
+    }
+    else {
+        tile.classList.add("blue-piece");
+        currPlayer = playerRed;
+    }
+
+    r -= 1;
+    currColumns[c] = r;
+
+    checkWinner();
+    switchTurn();
+}
+
+function checkWinner() {
+    // horizontal
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++){
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+        }
+   }
+
+   // vertical
+   for (let c = 0; c < columns; c++) {
+       for (let r = 0; r < rows - 3; r++) {
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+       }
+   }
+
+   // anti diagonal
+   for (let r = 0; r < rows - 3; r++) {
+       for (let c = 0; c < columns - 3; c++) {
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+       }
+   }
+
+   // diagonal
+   for (let r = 3; r < rows; r++) {
+       for (let c = 0; c < columns - 3; c++) {
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+       }
+   }
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("winner");
+    if (board[r][c] == playerRed) {
+        winner.innerText = "Red Wins";
+        winner.style.color = "red";            
+    } else {
+        winner.innerText = "Blue Wins";
+        winner.style.color = "blue";
+    }
+    gameOver = true;
+}
+
+let isPlayer1Turn = true;
+
+function updatePlayerTurnMessage() {
+    const playerTurnMessage = document.getElementById("playerTurn");
+    playerTurnMessage.textContent = isPlayer1Turn ? "Player 1's turn" : "Player 2's turn";
+    playerTurnMessage.style.color = isPlayer1Turn ? "#ff4747" : "#1E90FF";
+}
+
+function switchTurn() {
+    isPlayer1Turn = !isPlayer1Turn;
+    updatePlayerTurnMessage();
+}
